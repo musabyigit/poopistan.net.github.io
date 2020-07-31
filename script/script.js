2730564a5b41207feead199e1c27a73b584b3b78
= <=>
2020-07-31 18:30:41 +0300

    added css-js-html files

---
+++ script/script.js
@@ -0,0 +1,164 @@
+function ghost(){
+    var header= 'HAYALET BOK';
+    var text= 'Sıçtığınız zannedip de tuvalete baktığınızda göremediğiniz bok.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+
+function clean(){
+    var header= 'TEMİZ BOK';
+    var text= 'Sıçıp tuvalette gördüğünüz ama g*tünüze bulaşmayan bok';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;    
+}
+function soggy(){
+    var header= 'VICIK BOK';
+    var text= 'Özellikle eğer batı dünyasının taharet musluğuna sahip olmayan tuvaletlerinden birindeyseniz 50 kere sildikten sonra bile hala g*tünüzde kalan bok çeşidi. Eğer g*tünüzü yıkamama i̇mkanına sahip değilseniz, g*tünüzle donunuz arasına tuvalet kağıdı koymanıza neden olan bok';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;  
+}
+function secondWave(){
+    var header= 'İKİNCİ DALGA';
+    var text= 'Tam ayağa kalkmışken daha sıçasınız olduğunu anlamanıza neden olan bok';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function diet(){
+    var header= 'DİYET BOK';
+    var text= 'O kadar çok sıçarsınız ki 3-4 kilo kaybedersiniz. İşte o bok';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function anaconda(){
+    var header= 'ANACONDA';
+    var text= 'O kadar uzundurki, tuvalet fırçasıyla parçalara ayırmadan sifonu çekmeye korkarsınız. Çünkü sifondan gelen su anakonda ya çarpıp her tarafa sıçrayabilir.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function reverseFireworksDisplay(){
+    var header= 'TERSİNE HAYALET GÖSTERİSİ';
+    var text= 'Çok kısa bir zamanda i̇çinizdeki bok parçalarını bol gazla birlikte fışkırttığımız durum. Genelde tuvaletin her tarafının kirlenmesine ve "has s*ktir!" demenize sebep olur.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function egypt(){
+    var header= 'MISIR(EGYPT) BOK';
+    var text= 'Mısıra benzeyen bok. "ah keşke sıçabilseydim boku" bu bok g*tünüzden çıkmamakta israr eder ve uzun süre oturmanıza ve birkaç kere osurmanıza rağmen i̇çinizde kalır. Kabızlıkla yakından alakası olsa da kabız olmadığınız durumlarda da bu boktan üretebilirsiniz';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function iron(){
+    var header= 'DEMİR BOK';
+    var text= 'Çıkarken o kadar çok acıtır ki, çıkan şeyin demirden ve enlemesine çıkıyor olduğunu düşünürsünüz.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function assWetting(){
+    var header= 'G.T ISLATAN';
+    var text= ' Bu bok g*tünüzden o kadar şiddetli çıkar ki, tuvaletin suyu g*tünüzün her tarafını az çok islatacak şekilde sıçratır. Bu sıçışın ardından duş almak dinimizce tavsiye edilmektedi';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function liquid(){
+    var header= 'SIVI BOK';
+    var text= 'Adı üstünde g*tünüzden çıkan kahverengi sıvıyla tuvaletin her tarafını berbat etmenize neden olan bok. O kadar sıvıdır ki, bunla i̇şemenin mümkün olduğunu düşünebilirsiniz.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function abundantGarlicRumen(){
+    var header= 'BOL SARIMSAKLI İŞKEMBE';
+    var text= 'O kadar kötü kokar ki, evin tamamını havalandırmak zorunda kalırsınız. Bu bokun muhtemel nedeni i̇çerken yediğiniz bol sarımsaklı mezenin üstüne i̇çtiğiniz bol sarımsaklı i̇şkembe çorbasıdır.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function highSociety(){
+    var header= 'SOSYETE';
+    var text= 'Koca cüssesine rağmen kokmadığı düşünülen bok';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function littleCutePipildeks(){
+    var header= 'KÜÇÜK TATLI PİPİLDEKLER';
+    var text= 'Özellikle kamuya açık tuvaletlerde sıçtığınızda, defalarca sifonu çekmenize neden olan bok.i̇ki üç kere sifonu çekmenize rağmen, hala bazı küçük top gibi parçacıkların suda yüzdüğünü görürsünüz.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function rebellious(){
+    var header= 'İSYANKAR';
+    var text= 'Bu osurmaya çalışırken kaçırdığımız bok verilen i̇simdir. Genelde evinizde değilken başınıza gelir ve donunuza bulaştığı i̇çin bütün gün sizi rahatsız eder.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function commando(){
+    var header= 'KOMANDO';
+    var text= 'Kıçınızı temizlerken, g*tünüzün etrafındaki kıllara asılı kalarak düşmemeyi başarmış olan lokum kıvamında, fındık büyüklüğünde bok parçasına verilen i̇sim.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function glacier(){
+    var header= 'BUZUL';
+    var text= 'Yarısı suyun i̇çinde yarısı da suyun dışında duran bok.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function shot(){
+    var header= 'GÜLLE';
+    var text= 'Çok kuvvetli bir osurukla birlikte g*tünüzden fırlayan büyük bok parçasına verilen i̇sim. Genelde klozetin kırılıp kırılmadığını kontrol etmenize neden olur.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function playful(){
+    var header= 'ŞAKACI';
+    var text= 'G*tünüzden çıkar gibi olur, fakat çıktığını sanıp gevşediğinizde geri girer. Yarısı gözükür,sonra tekrar i̇çeri girer. Bu boku çıkarmak çok zordur. En i̇yi yöntem yarısını çıkarmayı başarınca çıkan kısmını tuvalet kağıdıyla silmek ve fazla zorlamamaktır.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function goat(){
+    var header= 'KEÇİ(GOAT)';
+    var text= 'Küçük küçük parçalar halinde çıkan ama sıç sıç bitmeyen bok.bir nokta da sıkıntıdan sıçmayı bırakırsınız, çünkü hiç bitmeyecek gibidir.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function symbol(){
+    var header= 'SİMGE';
+    var text= 'G*tünüzden yavaş yavaş çıkar ve yayıla yayıla serilir. Kalkıp baktığınızda kesin bir harfe ya da simgeye benzetirsiniz.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function plug(){
+    var header= 'TIKAÇ';
+    var text= 'Sıçtığınızda tuvaleti tıkayan, sifonu çekince de suyun taşmasına neden olan nadir bok çeşididir. Uzun süreli kabızlıktan sonra i̇yi bir sıçış bu etkiyi gösterebilir.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function shy(){
+    var header= 'UTANGAÇ';
+    var text= 'Sıçtıktan sonra doğrudan tuvalet deliğinden geçip gözlerden kaybolan bok.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function stalactite(){
+    var header= 'SARKIT';
+    var text= 'Sıçarken g*tünüzden ayrılmayan,bir süre asılı kalan komando özentisi bok. Biraz sallanarak düşürmeye çalışabiliriz.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function asocial(){
+    var header= 'ASOSYAL';
+    var text= 'Tam sıçarken,tuvaletin kapısının önünde birileri olduğunu hissettiğinizde i̇çeri kaçan bok';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function tits(){
+    var header= 'UYUŞTURUCU(TITS)';
+    var text= 'Çok uzun sürede çıktığından ayaklarınızın uyuşmasına neden olan bok.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+function thunder(){
+    var header= 'GÖK GÜRÜLTÜSÜ';
+    var text= 'Bu bok g*tünüzden çıkarken o kadar çok ses çıkartır ki hem i̇nanmayıp gülümsersiniz, hem de ev ahalisine rezil olduğunuz i̇çin kontrol etmeye çalışırsınız fakat kontrol etmeye çalıştıkça daha çeşitli sesler çıkarabildiğinizi anlayıp daha çok gülümsersiniz.';
+    document.getElementById('baslik').innerHTML = header;
+    document.getElementById('baslik2').innerHTML = text;
+}
+
